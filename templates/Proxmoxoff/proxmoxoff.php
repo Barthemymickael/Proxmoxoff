282c4874fd4fd93126d33527637a0bdbf70a0fbc
Barthemymickael <72488225+Barthemymickael@users.noreply.github.com>
2022-02-28 12:11:33 +0100

    'ajout'

---
+++ templates/Proxmoxoff/proxmoxoff.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+ 
+  <head>
+    <metacharset="utf-8">
+    <title>Projet Proxmox</title>
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+  </head>
+ 
+  <body>
+    <center><h1>Projet Proxmox</h1></center>
+    <div class="menu">
+<p>image et informtion</p>
+</div>
+    <div class="menu-g">
+    <h2>menu</h2>
+</div>
+
+    <div class="menu-d">
+<h3>Page qui change en fonction du clique sur un bouton</h3>
+</div>
+  </body>
+ 
+</html>
