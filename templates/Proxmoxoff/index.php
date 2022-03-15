d23c7962d93912c38ac5be9342f6d0c15821d6aa
Barthemymickael <72488225+Barthemymickael@users.noreply.github.com>
2022-03-15 11:41:50 +0100

    '15/03'

--- templates/Proxmoxoff/index.php
+++ templates/Proxmoxoff/index.php
@@ -1,24 +1,35 @@
 <!DOCTYPE html>
 <html>
- 
+
   <head>
     <metacharset="utf-8">
     <title>Projet Proxmox</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link href="css/styles.css" rel="stylesheet" type="text/css">
   </head>
  
-  <body>
+  <body style="background-color: #eeeeee">
+  
     <center><h1>Projet Proxmox</h1></center>
     <div class="menu">
+    <img class="prox"
+     src="/webroot/proxmox.png"
+     alt="Image de proxmox">
 <p>image et informtion</p>
 </div>
     <div class="menu-g">
-    <h2>menu</h2>
-</div>
+    <h2><center>menu<center></h2>
+    <a class="MuiListItem-root jss4 MuiListItem-gutters Mui-selected jss6" href="Serveur"><div class="MuiBox-root jss40"><svg class="MuiSvgIcon-root MuiBox-root jss41 jss12" focusable="false" viewBox="0 0 70 24" aria-hidden="true"><path d="M21 3H3c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h5v2h8v-2h5c1.1 0 1.99-.9 1.99-2L23 5c0-1.1-.9-2-2-2zm0 14H3V5h18v12z"></path></svg></div></a>
+    <a class="MuiListItem-root jss4 MuiListItem-gutters" href="VM"><div class="MuiBox-root jss42"><svg class="MuiSvgIcon-root MuiBox-root jss43 jss12" focusable="false" viewBox="0 0 70 24" aria-hidden="true"><path d="M10 12c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zM6 8c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 8c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm12-8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm-4 8c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm4-4c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm-4-4c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm-4-4c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"></path></svg></div></a>
+  </div>
 
     <div class="menu-d">
 <h3>Page qui change en fonction du clique sur un bouton</h3>
 </div>
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
   </body>
  
 </html>
